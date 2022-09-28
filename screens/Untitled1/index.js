b7e84304a1572da8405aab42185b89911b046aea
Ravinder yadav <ravinder.y@crowdbotics.com>
2022-09-28 13:36:59 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -3,7 +3,7 @@ import { View, StyleSheet } from "react-native";
 
 const Untitled1 = () => {
   return <View style={_styles.TLsTWqyf}>
-      </View>;
+      <View style={_styles.OVsSEVpo}></View></View>;
 };
 
 export default Untitled1;
@@ -14,5 +14,32 @@ const _styles = StyleSheet.create({
     padding: 10,
     position: "relative",
     height: "100%"
+  },
+  bvlnTtBe: {
+    left: 157.97101449275362,
+    top: 140.57971014492753,
+    position: "absolute",
+    width: 100,
+    height: 150
+  },
+  aVJOmMuJ: {
+    width: 92,
+    height: 60,
+    backgroundColor: "#FFFFFF"
+  },
+  kakswhJn: {
+    backgroundColor: "#000000",
+    height: 1
+  },
+  OVsSEVpo: {
+    left: 94,
+    top: 113,
+    position: "absolute",
+    height: 80,
+    width: 146,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777",
+    transform: "rotate(100deg)"
   }
 });
--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,9 +1,13 @@
+import { Slider } from "react-native-elements";
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
 const Untitled2 = () => {
   return <View style={_styles.lRwaILIj}>
-      </View>;
+      <Slider style={_styles.gPDVbjHL} thumbStyle={{
+      height: 20,
+      width: 20
+    }} thumbTintColor="#0000FF" maximumValue={7} minimumValue={1} value={16} step={7} disabled={true}></Slider></View>;
 };
 
 export default Untitled2;
@@ -14,5 +18,19 @@ const _styles = StyleSheet.create({
     padding: 10,
     position: "relative",
     height: "100%"
+  },
+  gPDVbjHL: {
+    left: 147.82608695652175,
+    top: 224.63768115942028,
+    position: "absolute",
+    width: 150,
+    height: 40
+  },
+  oxqyvmBU: {
+    left: 46,
+    top: 117,
+    position: "absolute",
+    width: 183,
+    height: 69
   }
 });
